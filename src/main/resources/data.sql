72551da29b2c65ffa14e3ca5e7edcaf353d3eb30
이승환 <lsh01087674117@gmail.com>
2022-02-23 00:21:38 +0900

    초기 data 셋팅을 위한 쿼리추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
-INSERT INTO join_available (idx, name, reg_no)
+INSERT INTO user (user_idx, name, password, reg_no, user_id)
+VALUES (1, '홍길동', '123', '860824-1655068', 1);
+
+INSERT INTO join_available (available_idx, name, reg_no)
 VALUES (1, '홍길동', '860824-1655068'),
        (2, '김둘리', '921108-1582816'),
        (3, '마징가', '880601-2455116'),
