5f08b03ca1c7a2402c9b8817c6a2ac96a16e092a
이승환 <lsh01087674117@gmail.com>
2022-02-24 01:30:19 +0900

    암호화 유틸 추가

---
+++ src/main/java/com/example/project/util/AESCryptoUtil.java
@@ -0,0 +1,56 @@
+package com.example.project.util;
+
+import org.springframework.stereotype.Service;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+/**
+ * @author 이승환
+ * @since 2022-02-23
+ */
+@Service
+public class AESCryptoUtil {
+
+    public static String alg = "AES/CBC/PKCS5Padding";
+    private final String key = "01234567890123456789012345678901";
+    private final String iv = key.substring(0, 16); // 16byte
+
+    /**
+     * 암호화
+     *
+     * @param text
+     * @return
+     * @throws Exception
+     */
+    public String encrypt(String text) throws Exception {
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+        byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    /**
+     * 복호화
+     *
+     * @param cipherText
+     * @return
+     * @throws Exception
+     */
+    public String decrypt(String cipherText) throws Exception {
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+        byte[] decrypted = cipher.doFinal(decodedBytes);
+        return new String(decrypted, "UTF-8");
+    }
+
+}
