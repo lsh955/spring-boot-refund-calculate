e53e7eebe79245955fd725d8455558847b4d6325
이승환 <lsh01087674117@gmail.com>
2022-02-24 01:31:22 +0900

    스크랩 enum 클래스 추가

---
+++ src/main/java/com/example/project/enums/ScrapStatus.java
@@ -0,0 +1,29 @@
+package com.example.project.enums;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+/**
+ * @author 이승환
+ * @since 2022-02-23
+ */
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ScrapStatus {
+
+    NO_SCRAP_DATA("SCRAP_ERR_0001", "저장된 데이터가 없습니다.");
+
+    private final String code;
+    private final String message;
+
+    ScrapStatus(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
