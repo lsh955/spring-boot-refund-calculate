3abd7d2c7490bb334b3182c6cf8957647bbb6b9a
이승환 <lsh01087674117@gmail.com>
2022-02-24 01:29:28 +0900

    민감정보는 사전에 암호화처리

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,10 @@
+-- TODO :: 완성 후 user 테이블 INSERT 쿼리 지울것
 INSERT INTO user (user_idx, name, password, reg_no, user_id)
-VALUES (1, '홍길동', '123', '860824-1655068', 1);
+VALUES (1, '홍길동', 'ELbbqFzaPvFZbCrhd61Mzw==', 'ldU2Z5ZlRuwPfYA1YfvOTw==', 1);
 
 INSERT INTO join_available (available_idx, name, reg_no)
-VALUES (1, '홍길동', '860824-1655068'),
-       (2, '김둘리', '921108-1582816'),
-       (3, '마징가', '880601-2455116'),
-       (4, '배지터', '910411-1656116'),
-       (5, '손오공', '820326-2715702');
+VALUES (1, '홍길동', 'ldU2Z5ZlRuwPfYA1YfvOTw=='),
+       (2, '김둘리', 'U99p1DIkTEpARHoYcosMfA=='),
+       (3, '마징가', 'hdK2SQrCrpqBf+wgoW2jEQ=='),
+       (4, '배지터', 'DqZxJbXxfYcObBUCQ+Xfag=='),
+       (5, '손오공', 'eeXoFpR60+NfeIpj4aXnWw==');
