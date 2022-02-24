6ee4313ad42c2b64cc4666672c34d601c09b5577
이승환 <lsh01087674117@gmail.com>
2022-02-24 19:04:04 +0900

    테스트코드에 대한 초기셋팅 추가

---
+++ src/test/java/com/example/project/repository/scrap/ScrapOneRepositoryTest.java
@@ -0,0 +1,8 @@
+package com.example.project.repository.scrap;
+
+/**
+ * @author 이승환
+ * @since 2022-02-24
+ */
+class ScrapOneRepositoryTest {
+}
---
+++ src/test/java/com/example/project/service/AccountServiceTest.java
@@ -0,0 +1,8 @@
+package com.example.project.service;
+
+/**
+ * @author 이승환
+ * @since 2022-02-24
+ */
+class AccountServiceTest {
+}
