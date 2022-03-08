4ab42b81d65b0495c042e6e00c88e338c3b7455a
이승환 <lsh01087674117@gmail.com>
2022-03-08 11:57:47 +0900

    주석보강 및 정렬

--- src/main/java/com/example/project/util/AESCryptoUtil.java
+++ src/main/java/com/example/project/util/AESCryptoUtil.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 /**
@@ -19,7 +20,7 @@ public class AESCryptoUtil {
     private final String iv = key.substring(0, 16); // 16byte
 
     /**
-     * 암호화
+     * 암호화 처리
      *
      * @param text
      * @return
@@ -31,12 +32,13 @@ public class AESCryptoUtil {
         IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
         cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
 
-        byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+        byte[] encrypted = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+
         return Base64.getEncoder().encodeToString(encrypted);
     }
 
     /**
-     * 복호화
+     * 복호화 처리
      *
      * @param cipherText
      * @return
@@ -50,7 +52,8 @@ public class AESCryptoUtil {
 
         byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
         byte[] decrypted = cipher.doFinal(decodedBytes);
-        return new String(decrypted, "UTF-8");
+
+        return new String(decrypted, StandardCharsets.UTF_8);
     }
 
 }
