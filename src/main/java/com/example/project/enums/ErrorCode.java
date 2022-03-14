3a4b785b01cbdb5d4a689b040ed0387854fc7e19
이승환 <lsh01087674117@gmail.com>
2022-03-14 09:54:16 +0900

    위치수정.

--- src/main/java/com/example/project/enums/ErrorCode.java
+++ src/main/java/com/example/project/enums/ErrorCode.java
@@ -21,16 +21,16 @@ public enum ErrorCode {
     REG_NO_OVERLAP(CONFLICT, "중복된 주민등록번호 입니다."),
     UNABLE_TO_NAME(CONFLICT, "가입 가능한 사용자이름이 아닙니다"),
     UNABLE_TO_REG_NO(CONFLICT, "가입 가능한 주민번호가 아닙니다"),
+    DUPLICATE_RESOURCE(CONFLICT, "데이터가 이미 존재합니다"),
 
     // 401
     INVALID_AUTH_TOKEN(UNAUTHORIZED, "권한 정보가 없는 토큰입니다"),
     UNAUTHORIZED_MEMBER(UNAUTHORIZED, "현재 정보가 존재하지 않습니다"),
     UNAUTHORIZED_PASSWORD(UNAUTHORIZED, "패스워드가 올바르지 않습니다."),
 
-    // 404
+    // 404,
     NO_SCRAP_DATA(NOT_FOUND, "요청한 데이터가 존재하지 않습니다."),
-    MEMBER_NOT_FOUND(NOT_FOUND, "사용자 정보를 찾을 수 없습니다"),
-    DUPLICATE_RESOURCE(CONFLICT, "데이터가 이미 존재합니다");
+    MEMBER_NOT_FOUND(NOT_FOUND, "사용자 정보를 찾을 수 없습니다");
 
     private final HttpStatus httpStatus;
     private final String detail;
