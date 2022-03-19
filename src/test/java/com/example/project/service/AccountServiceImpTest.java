2a31cb7f25bba2911e79016d88502d738ad039ef
이승환 <lsh01087674117@gmail.com>
2022-03-19 14:32:40 +0900

    회원가입 정상등록되는 테스트케이스 작성.

--- src/test/java/com/example/project/service/AccountServiceImpTest.java
+++ src/test/java/com/example/project/service/AccountServiceImpTest.java
@@ -1,8 +1,10 @@
 package com.example.project.service;
 
 import com.example.project.app.account.domain.JoinAvailableRepository;
+import com.example.project.app.account.domain.User;
 import com.example.project.app.account.domain.UserRepository;
 import com.example.project.app.account.service.AccountServiceImp;
+import com.example.project.app.common.enums.AccountStatus;
 import com.example.project.app.common.enums.ErrorCode;
 import com.example.project.app.common.util.AESCryptoUtil;
 import com.example.project.exception.CustomException;
@@ -15,7 +17,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 /**
  * @author 이승환
@@ -73,5 +76,34 @@ class AccountServiceImpTest {
 
         // then
         assertThat(result.getErrorCode()).isEqualTo(ErrorCode.REG_NO_OVERLAP);
+    }
+    
+    @Test
+    @DisplayName("회원가입 정상등록")
+    public void addSignup () throws Exception {
+        // given
+        doReturn(true).when(joinAvailableRepository).existsByRegNo(encryptedRegNo);
+        doReturn(false).when(userRepository).existsByRegNo(encryptedRegNo);
+        doReturn(encryptedRegNo).when(aesCryptoUtil).encrypt(regNo);
+        doReturn(user()).when(userRepository).save(any(User.class));
+            
+        // when
+        final AccountStatus result = accountServiceImp.addSignup(userId, password, name, regNo);
+            
+        // then
+        assertThat(result.getCode()).isEqualTo(AccountStatus.SIGNUP_SUCCESS.getCode());
+        assertThat(result.getMessage()).isEqualTo(AccountStatus.SIGNUP_SUCCESS.getMessage());
+
+        // verify
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    private User user() {
+        return User.builder()
+                .userId("1")
+                .password("ELbbqFzaPvFZbCrhd61Mzw==")
+                .name("홍길동")
+                .regNo("ldU2Z5ZlRuwPfYA1YfvOTw==")
+                .build();
     }
 }
