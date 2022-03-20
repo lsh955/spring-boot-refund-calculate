983c0c7175998b6df6f47914d147facab104be49
이승환 <lsh01087674117@gmail.com>
2022-03-20 16:58:11 +0900

    테스트케이스에 에러에 따른 수정.

--- src/main/java/com/example/project/app/account/controller/AccountController.java
+++ src/main/java/com/example/project/app/account/controller/AccountController.java
@@ -7,6 +7,7 @@ import com.example.project.app.common.enums.AccountStatus;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author 이승환
  * @since 2022-02-18
- *
+ * <p>
  * 계정에 관련된 Controller
  */
 @RestController
@@ -28,26 +29,28 @@ public class AccountController {
     /**
      * 회원가입 등록
      *
-     * @param userDto   userDto
-     * @return          message
+     * @param userDto userDto
+     * @return message
      */
     @ApiOperation(value = "회원가입", notes = "입력에 따른 사용자 정보등록")
     @PostMapping(value = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public AccountStatus signup(@RequestBody UserDto userDto) throws Exception {
+    public ResponseEntity<AccountStatus> signup(@RequestBody UserDto userDto) throws Exception {
 
-        return accountService.addSignup(
+        final AccountStatus accountStatus = accountService.addSignup(
                 userDto.getUserId(),
                 userDto.getPassword(),
                 userDto.getName(),
                 userDto.getRegNo()
         );
+
+        return ResponseEntity.ok(accountStatus);
     }
 
     /**
      * 로그인
      *
-     * @param userDto   userDto
-     * @return          User Token
+     * @param userDto userDto
+     * @return User Token
      */
     @ApiOperation(value = "로그인", notes = "사용자검증에 따른 토큰발급")
     @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -62,8 +65,8 @@ public class AccountController {
     /**
      * 개인정보 보기
      *
-     * @param jwtTokenDto   User Token
-     * @return              개인정보
+     * @param jwtTokenDto User Token
+     * @return 개인정보
      */
     @ApiOperation(value = "개인정보 보기", notes = "토큰정보에 따른 사용자 정보조회")
     @PostMapping(value = "/me", consumes = MediaType.APPLICATION_JSON_VALUE)
