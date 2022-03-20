a5c8a5597b66bdb67f46bb1ce30a1b4419e62b80
이승환 <lsh01087674117@gmail.com>
2022-03-20 16:57:40 +0900

    회원가입에 대한 테스트케이스 추가.

--- src/test/java/com/example/project/controller/AccountControllerTest.java
+++ src/test/java/com/example/project/controller/AccountControllerTest.java
@@ -1,176 +1,116 @@
 package com.example.project.controller;
 
-import com.example.project.app.account.domain.UserRepository;
+import com.example.project.app.account.controller.AccountController;
 import com.example.project.app.account.dto.UserDto;
-import com.example.project.app.common.enums.AccountStatus;
-import com.example.project.app.common.util.JwtTokenUtil;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.AfterEach;
+import com.example.project.app.account.service.AccountServiceImp;
+import com.example.project.app.common.enums.ErrorCode;
+import com.example.project.exception.CustomException;
+import com.example.project.exception.GlobalExceptionHandler;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.HashMap;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author 이승환
  * @since 2022-02-19
  */
-@AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(MockitoExtension.class)
 class AccountControllerTest {
 
-    @Autowired
+    @InjectMocks
+    private AccountController accountController;
+
+    @Mock
+    private AccountServiceImp accountServiceImp;
+
     private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
 
-    @AfterEach
-    public void down() {
-        userRepository.deleteAll();
+    private Gson gson;
+
+    @BeforeEach
+    private void init() {
+        gson = new Gson();
+        mockMvc = MockMvcBuilders.standaloneSetup(accountController)
+                .setControllerAdvice(GlobalExceptionHandler.class)
+                .build();
     }
 
-    @Test
-    @DisplayName(value = "회원가입 등록성공(가입가능한, 최초 가입하는 경우)")
-    public void SIGNUP_SUCCESS_TEST() throws Exception {
-        String userId = "2";
-        String password = "123";
-        String name = "김둘리";
-        String regNo = "921108-1582816";
-
-        UserDto userDto = UserDto.builder()
+    private UserDto userDto(String userId, String password, String name, String regNo) {
+        return UserDto.builder()
                 .userId(userId)
                 .password(password)
                 .name(name)
                 .regNo(regNo)
                 .build();
-
-        this.mockMvc.perform(post("http://localhost:8080/szs/signup")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.objectMapper.writeValueAsString(userDto)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("code").value(AccountStatus.SIGNUP_SUCCESS.getCode()))
-                .andExpect(jsonPath("message").value(AccountStatus.SIGNUP_SUCCESS.getMessage()));
     }
 
     @Test
-    @DisplayName(value = "회원가입 등록실패(화원가입이 이미 되어있는 경우)")
-    public void SIGNUP_FAILURE_TEST() throws Exception {
-        String userId = "1";
-        String password = "123";
-        String name = "홍길동";
-        String regNo = "860824-1655068";
-
-        UserDto userDto = UserDto.builder()
-                .userId(userId)
-                .password(password)
-                .name(name)
-                .regNo(regNo)
-                .build();
-
-        this.mockMvc.perform(post("http://localhost:8080/szs/signup")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.objectMapper.writeValueAsString(userDto)))
-                .andExpect(status().is4xxClientError());
+    @DisplayName("가입가능한 주민등록번호가 없을시")
+    public void 가입가능한_주민등록번호가_없을시 () throws Exception {
+        // given
+        final String url = "/szs/signup";
+        doThrow(new CustomException(ErrorCode.UNABLE_TO_REG_NO))
+                .when(accountServiceImp)
+                .addSignup("2", "123", "이승환", "123456-789456");
+            
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                .content(gson.toJson(userDto("2", "123", "이승환", "123456-789456")))
+                .contentType(MediaType.APPLICATION_JSON)
+        );
+            
+        // then
+        resultActions.andExpect(status().isConflict());
     }
 
     @Test
-    @DisplayName(value = "회원가입 등록실패(회원가입 가능한 유저가 아닌경우)")
-    public void SIGNUP_UNABLE_FAILURE_TEST() throws Exception {
-        String userId = "3";
-        String password = "123";
-        String name = "이승환";
-        String regNo = "123456-789456";
+    @DisplayName("가입된 주민등록번호가 이미 있을시")
+    public void 가입된_주민등록번호가_이미_있을시 () throws Exception {
+        // given
+        final String url = "/szs/signup";
+        doThrow(new CustomException(ErrorCode.REG_NO_OVERLAP))
+                .when(accountServiceImp)
+                .addSignup("2", "123", "이승환", "860824-1655068");
 
-        UserDto userDto = UserDto.builder()
-                .userId(userId)
-                .password(password)
-                .name(name)
-                .regNo(regNo)
-                .build();
-
-        this.mockMvc.perform(post("http://localhost:8080/szs/signup")
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .content(gson.toJson(userDto("2", "123", "이승환", "860824-1655068")))
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.objectMapper.writeValueAsString(userDto)))
-                .andExpect(status().is4xxClientError());
+        );
+
+        // then
+        resultActions.andExpect(status().isConflict());
     }
 
     @Test
-    @DisplayName(value = "로그인 성공(올바른 정보로 로그인한 경우)")
-    public void LOGIN_SUCCESS_TEST() throws Exception {
-        String userId = "1";
-        String password = "123";
+    @DisplayName("회원가입 성공")
+    public void 회원가입_성공 () throws Exception {
+        // given
+        final String url = "/szs/signup";
 
-        String name = "홍길동";
-        String regNo = "ldU2Z5ZlRuwPfYA1YfvOTw==";
-        HashMap<String, String> token = this.jwtTokenUtil.createToken(name, regNo);
-
-        UserDto userDto = UserDto.builder()
-                .userId(userId)
-                .password(password)
-                .build();
-
-        this.mockMvc.perform(post("http://localhost:8080/szs/login")
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .content(gson.toJson(userDto("2", "123", "이승환", "123456-789456")))
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.objectMapper.writeValueAsString(userDto)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("token").value(token.get("token")));
-    }
+        );
 
-    @Test
-    @DisplayName(value = "로그인 실패(올바른 정보로 로그인 하지 않는 경우)")
-    public void LOGIN_FAILURE_TEST() throws Exception {
-        String userId = "6";
-        String password = "1234";
-
-        UserDto userDto = UserDto.builder()
-                .userId(userId)
-                .password(password)
-                .build();
-
-        this.mockMvc.perform(post("http://localhost:8080/szs/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.objectMapper.writeValueAsString(userDto)))
-                .andExpect(status().is4xxClientError());
-    }
-
-    @Test
-    @DisplayName(value = "개인정보 보기 성공")
-    public void ME_SUCCESS_TEST() throws Exception {
-        String name = "홍길동";
-        String regNo = "ldU2Z5ZlRuwPfYA1YfvOTw==";
-
-        HashMap<String, String> token = this.jwtTokenUtil.createToken(name, regNo);
-
-        this.mockMvc.perform(post("http://localhost:8080/szs/me")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.objectMapper.writeValueAsString(token)))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName(value = "개인정보 보기 실패")
-    public void ME_FAILURE_TEST() throws Exception {
-        String name = "손오공";
-        String regNo = "eeXoFpR60+NfeIpj4aXnWw==";
-
-        HashMap<String, String> token = this.jwtTokenUtil.createToken(name, regNo);
-
-        this.mockMvc.perform(post("http://localhost:8080/szs/me")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.objectMapper.writeValueAsString(token)))
-                .andExpect(status().is4xxClientError());
+        // then
+        resultActions.andExpect(status().isOk());
     }
 }
