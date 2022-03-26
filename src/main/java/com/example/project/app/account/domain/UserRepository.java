8b23e7a5739e809395e18cde8bfcc148b189ba51
이승환 <lsh01087674117@gmail.com>
2022-03-26 11:27:00 +0900

    orElseThrow 으로 리펙토링에 따른 처리

--- src/main/java/com/example/project/app/account/domain/UserRepository.java
+++ src/main/java/com/example/project/app/account/domain/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * @author 이승환
  * @since 2022-02-18
@@ -17,7 +19,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUserId(String userId);
 
-    User findByNameAndRegNo(String name, String regNo);
+    Optional<User> findByNameAndRegNo(String name, String regNo);
 
     @Query(
             value = "	SELECT user_idx " +
